616b5b33c5de11afa3a2979ee9221a6a9873e8e3
Arthur797 <aburlinchon2@gmail.com>
2022-05-26 11:58:27 +0200

    ajout1 arthur

---
+++ CompteCoach.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Salles de Sport</title>
+    <meta charset="uft-8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="omnes.jfif"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="http://localhost/Projet_Web/Accueil.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Accueil</a>
+                    </li><br>
+                    <li class="nav-item">
+                        <a href="http://localhost/Projet_Web/toutparcourir.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true"> Tout Parcourir</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Rendez-Vous</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Recherche</button>
+                </form>
+                <a href="http://localhost/Projet_Web/SeConnecter.php" class="btn active" role="button" aria-pressed="true"><img src="votrecompte.png" alt="Compte" height="150" width="150"></a>
+            </div>
+        </div>
+    </nav>
+    <div id="wrapper4admin">
+        <div class="coach">
+            <h2 class="display-4">Compte Coach</h2>
+            <p class="lead">Nom</p>
+            <p class="lead">Prenom</p>
+            <hr class="my-4">
+            <p>Mail</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="http://localhost/Projet_Web/ChatCoach.php" role="button">Chatbox</a>
+                <a class="btn btn-primary btn-lg" href="http://localhost/Projet_Web/RdvCoach.php" role="button">Liste des Rendez-vous</a>
+            </p>
+        </div>
+    </div>
+
+</body>
+
+</html>
---
+++ CreerCoach.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Connexion</title>
+    <meta charset="uft-8" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <a href="#"><img src="omnes.jfif"></a>
+    <h1>Ajouter un coach</h1>
+    <br>
+    <br>
+    <form>
+        <div class="form-row">
+            <div class="col">
+                <label for="inputFirstName">Prenom Coach</label>
+                <input type="text" class="form-control" id="inputFirstName" placeholder="Prenom">
+            </div>
+            <div class="col">
+                <label for="inputLastName">Nom Coach</label>
+                <input type="text" class="form-control" id="inputLastName" placeholder="Nom">
+            </div>
+        </div>
+        <div class="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+        </div>
+        <div class="form-group col-md-6">
+            <label for="inputPassword4">Mot de passe</label>
+            <input type="password" class="form-control" id="inputPassword4" placeholder="Mot de passe">
+        </div>
+        </div>
+        <div class="form-group col-md-2">
+            <label for="inputNumEtudiant">ID</label>
+            <input type="text" class="form-control" id="inputNumEtudiant" placeholder="1234 ">
+        </div>
+
+        <div class="form-row">
+            <div class="col">
+                <label class="mr-sm-2" for="specialite1">Specialite 1</label>
+                <select class="custom-select mr-sm-2" id="specialite1">
+                    <option selected>Choose...</option>
+                    <option value="1">Musculation</option>
+                    <option value="2">Fitness</option>
+                    <option value="3">Biking</option>
+                    <option value="4">Cardio-Training</option>
+                    <option value="5">Cours collectif</option>
+                    <option value="6">Basketball</option>
+                    <option value="7">Football</option>
+                    <option value="8">Rugby</option>
+                    <option value="9">Tenis</option>
+                    <option value="10">Natation</option>
+                    <option value="11">Plongeon</option>
+                </select>
+            </div>
+            <div class="col">
+                <label class="mr-sm-2" for="specialite2">Specialite 2</label>
+                <select class="custom-select mr-sm-2" id="specialite2">
+                    <option selected>Choose...</option>
+                    <option value="1">Musculation</option>
+                    <option value="2">Fitness</option>
+                    <option value="3">Biking</option>
+                    <option value="4">Cardio-Training</option>
+                    <option value="5">Cours collectif</option>
+                    <option value="6">Basketball</option>
+                    <option value="7">Football</option>
+                    <option value="8">Rugby</option>
+                    <option value="9">Tenis</option>
+                    <option value="10">Natation</option>
+                    <option value="11">Plongeon</option>
+                    <option value="12">-Pas d'autre Specialite-</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group col-md-6">
+            <label for="inputphoto">Url photo</label>
+            <input type="text" class="form-control" id="inputPhoto" placeholder="Photo">
+        </div>
+        <div class="form-group">
+            <div class="form-check3">
+                <a class="btn btn-outline-secondary" href="http://localhost/Projet_Web/CompteAdmin.php" role="button">Ajouter</a>
+            </div>
+        </div>
+    </form>
+
+</body>
