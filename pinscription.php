be0c113ee517cbffb221671022af585fc8e0da1b
Arthur797 <aburlinchon2@gmail.com>
2022-05-29 18:49:16 +0200

    pinscription (correction erreurs)

--- pinscription.php
+++ pinscription.php
@@ -18,67 +18,44 @@
     <?php include 'include/menu_navigation_principal.php'; ?>
     <?php include 'include/menu_navigation_moncompte.php'; ?>
     <?php include 'include/database.php' ?>
-    <?php include 'include/menu_nav.php'; ?>
+    <?php include 'include/menu_nav.php'; ?><br><br><br><br><br><br><br>
+
     <form method="post">
         <table>
             <p>
             <h1>Clients</h1>
             </p>
             <tr>
-                <div class="row">
-                    <div class="col-md-2 mb-3">
-                        <label for="nom">Nom</label>
-                        <input type="text" class="form-control" id="nom" placeholder="" value="" required>
-                    </div>
-                    <div class="col-md-2 mb-3">
-                        <label for="prenom">Prenom</label>
-                        <input type="text" class="form-control" id="prenom" placeholder="" value="" required>
-                    </div>
-                </div>
+                <td>Nom:</td>
+                <td><input type="text" step="0.01" name="nom" id="nom"></td>
+                <td>Prénom:</td>
+                <td><input type="text" step="0.01" name="prenom" id="prenom"></td>
             </tr>
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="email">Email <span class="text-muted">(Optional)</span></label>
-                    <input type="email" class="form-control" id="email" placeholder="you@example.com">
-                </div>
+                <td>Email:</td>
+                <td><input type="email" step="0.01" name="email" id="email"></td>
             </tr>
 
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="born">Naissance</label>
-                    <input type="date" class="form-control" id="born" placeholder="you@example.com">
-                </div>
+                <td>Naissance:</td>
+                <td><input type="date" step="0.01" name="born" id="born"></td>
             </tr>
 
             <tr>
-                <div class="row">
-
-                    <div class="col-md-3 mb-3">
-                        <label for="num_carte">Credit card number</label>
-                        <input type="number" class="form-control" id="num_carte" placeholder="" required>
-                    </div>
-                    <div class="col-md-2 mb-3">
-                        <label for="exp_carte">Expiration</label>
-                        <input type="month" class="form-control" id="exp_carte" placeholder="" required>
-                    </div>
-                    <div class="col-md-2 mb-3">
-                        <label for="cvv_carte">CVV</label>
-                        <input type="number" class="form-control" id="cvv_carte" placeholder="" required>
-
-                    </div>
-                </div>
+                <td>Numéro de carte:</td>
+                <td><input type="number" step="0.01" name="num_carte" id="num_carte"></td>
+                <td>Date d'expiration:</td>
+                <td><input type="month" step="0.01" name="exp_carte" id="exp_carte"></td>
+                <td>CVV:</td>
+                <td><input type="number" step="0.01" name="cvv_carte" id="cvv_carte"></td>
             </tr>
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="mp">Mot de passe</label>
-                    <input type="password" class="form-control" id="mp" placeholder="">
-                </div>
-            </tr>           
+                <td>Mot de passe:</td>
+                <td><input type="password" step="0.01" name="mp" id="mp"></td>
+            </tr>
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="cmp">Confirmation Mot de passe</label>
-                    <input type="password" class="form-control" id="cmp" placeholder="">
-                </div>
+                <td>Confirmation mot de passe:</td>
+                <td><input type="password" step="0.01" name="cmp" id="cmp"></td>
             </tr>
             <tr>
                 <td colspan="2" align="center">
@@ -89,41 +66,31 @@
     </form>
 
     <form method="post">
-        <table>
+        <table><br><br><br><br><br>
             <h1>
                 Administrateur
             </h1>
-
             <tr>
-                <div class="row">
-                    <div class="col-md-2 mb-3">
-                        <label for="anom">Nom</label>
-                        <input type="text" class="form-control" id="anom" placeholder="" value="" required>
-                    </div>
-                    <div class="col-md-2 mb-3">
-                        <label for="aprenom">Prenom</label>
-                        <input type="text" class="form-control" id="aprenom" placeholder="" value="" required>
-                    </div>
-                </div>
+                <td>Nom:</td>
+                <td><input type="text" step="0.01" name="anom" id="anom"></td>
             </tr>
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="aemail">Email <span class="text-muted">(Optional)</span></label>
-                    <input type="email" class="form-control" id="aemail" placeholder="you@example.com">
-                </div>
+                <td>Prénom:</td>
+                <td><input type="text" step="0.01" name="aprenom" id="aprenom"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="email" step="0.01" name="aemail" id="aemail"></td>
             </tr>
 
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="amp">Mot de passe</label>
-                    <input type="password" class="form-control" id="amp" placeholder="">
-                </div>
-            </tr>           
+                <td>Mot de passe:</td>
+                <td><input type="password" step="0.01" name="amp" id="amp"></td>
+            </tr>
+
             <tr>
-                <div class="col-md-6 mb-3">
-                    <label for="acmp">Confirmation Mot de passe</label>
-                    <input type="password" class="form-control" id="acmp" placeholder="">
-                </div>
+                <td>Confirmation mot de passe:</td>
+                <td><input type="password" step="0.01" name="acmp" id="acmp"></td>
             </tr>
             <tr>
                 <td colspan="2" align="center">
@@ -138,4 +105,5 @@
     <?php include 'include/footer.php'; ?>
 
 </body>
+
 </html>
